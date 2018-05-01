4b1739fd5324c45530a8232135ce58661d90323b
Kyle Furlong <35738072+krfurlong@users.noreply.github.com>
2018-05-01 12:33:31 -0400

    Update d3script.js

--- assets/dataViz/d3script.js
+++ assets/dataViz/d3script.js
@@ -8,7 +8,7 @@ d3.csv('assets/dataViz/data.csv',function (data) {
 
   // Select X-axis Variable
   var span = body.append('span')
-    .text('Select X-Axis variable: ')
+    .text('Select X-Axis variable2: ')
   var yInput = body.append('select')
       .attr('id','xSelect')
       .on('change',xChange)
