31e37ec06821620ec3d1841a1171636fdd7137d7
Kyle Furlong <35738072+krfurlong@users.noreply.github.com>
2018-05-02 15:58:57 -0400

    Update d3fifaheat2.js

--- assets/dataViz/d3fifaheat2.js
+++ assets/dataViz/d3fifaheat2.js
@@ -89,7 +89,8 @@ var margin = { top: 50, right: 0, bottom: 50, left: 100 },
 
           legend.append("text")
             .attr("class", "mono")
-            .text(function(d) { return "≥ " + Math.round(d); })
+            .text(function(d) { return Math.round(d); })
+            .style("fill", "white")
             .attr("x", function(d, i) { return legendElementWidth * i; })
             .attr("y", height + gridSize);
 
